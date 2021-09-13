60d75206ab484d4b7663a22cb008b76f9c2fd92b
Dario-B0 <dariobasile55@gmail.com>
2021-09-13 12:47:22 +0200

    fornite ulteriori api con documentazione

--- docker_update_ssl/app/api/documentation/api_project.js
+++ docker_update_ssl/app/api/documentation/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "1.0.0",
   "description": "Api documentation for Sapiens social network",
   "title": "Sapiens apidoc",
-  "url": "http://localhost:3001",
-  "sampleUrl": "http://localhost:3001",
+  "url": "http://localhost:8080",
+  "sampleUrl": "http://localhost:8080",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-09-12T20:10:52.188Z",
+    "time": "2021-09-13T10:42:41.092Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
