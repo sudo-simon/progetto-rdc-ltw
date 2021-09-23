b2c2437fc3788fd265ebef9dbab772ca4af64d71
Dario-B0 <dariobasile55@gmail.com>
2021-09-23 12:18:59 +0200

    documentaione api raggiungibile tramite url

--- app/api/public/apidoc/api_project.js
+++ app/api/public/apidoc/api_project.js
@@ -1,16 +1,22 @@
-{
+define({
   "name": "Sapiens",
   "version": "1.0.0",
   "description": "Api documentation for Sapiens social network",
   "title": "Sapiens apidoc",
-  "url": "http://localhost:8080",
+  "url": "http://localhost:3001",
   "sampleUrl": "http://localhost:8080",
+  "order": [
+    "User",
+    "Info",
+    "Activities",
+    "Social"
+  ],
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-09-13T10:42:41.092Z",
+    "time": "2021-09-23T10:16:51.610Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
-}
+});
